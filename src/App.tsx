6bc63abfae41aa50871600ab42833abeb3c51e1c
MariavCavaliere <arnal.maria00@gmail.com>
2025-10-18 11:35:59 -0400

    creando proyecto (react)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1>mi primer proyecto en react</h1>
+    <p>hecho por maria</p>
+   </div>
+  )
+}
+
+export default App
